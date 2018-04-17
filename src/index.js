3be08d0355af8f59c21b41b1e0fc9a60690eee52
RoyalSix <jay.a.scottjr@biola.edu>
2018-04-17 14:12:57 -0700

    changed export style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-const aligner = require('./js/aligner');
-const VerseObectUtils = require('./js/utils/verseObjects');
-const ArrayUtils = require('./js/utils/array');
+import * as aligner from './js/aligner';
+import * as VerseUtils from './js/utils/verseObjects';
+import * as ArrayUtils from './js/utils/array';
 
-export {VerseObectUtils, ArrayUtils};
-export {aligner as default};
+export default {
+  ...aligner,
+  ...VerseUtils,
+  ...ArrayUtils
+};
